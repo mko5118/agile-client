164abdac29bd74b7eff217df65ecd2aaf8341284
Min Ko <minpko@gmail.com>
2020-05-20 14:08:35 -0400

    Setup initial Client folder - React Frontend

---
+++ client/src/index.js
@@ -0,0 +1,11 @@
+import React, { } from 'react';
+import ReactDOM from 'react-dom';
+import './styles/styles.scss';
+
+import App from './routes/App';
+
+const jsx = (
+  <App />
+);
+
+ReactDOM.render(jsx, document.getElementById('root'));
---
+++ client/src/routes/App.js
@@ -0,0 +1,14 @@
+import React, { } from 'react';
+
+import style from './app.modules.scss';
+
+// *************************** APP COMPONENT *************************** //
+const App = () => {
+  return (
+    <div className={style.app}>
+      AGILE CLIENT WEB APP
+    </div>
+  )
+};
+
+export default App;
