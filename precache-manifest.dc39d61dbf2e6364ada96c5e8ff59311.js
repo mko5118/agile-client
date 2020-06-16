c6f78e849d525dfe069cf8bd31b2232277edd2f9
Min Ko <minpko@gmail.com>
2020-06-16 11:20:34 -0400

    Updates

---
+++ precache-manifest.dc39d61dbf2e6364ada96c5e8ff59311.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52a67b03ce22b1e3c5f170400ef3b570",
+    "url": "/agile-client/index.html"
+  },
+  {
+    "revision": "4d225d4d34e295910a28",
+    "url": "/agile-client/static/css/2.77b22cec.chunk.css"
+  },
+  {
+    "revision": "e5642f2f3f14ed21c1e6",
+    "url": "/agile-client/static/css/main.3dd999fe.chunk.css"
+  },
+  {
+    "revision": "4d225d4d34e295910a28",
+    "url": "/agile-client/static/js/2.78c8bed5.chunk.js"
+  },
+  {
+    "revision": "df86c357745285c7d56ae40b5421a8ca",
+    "url": "/agile-client/static/js/2.78c8bed5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e5642f2f3f14ed21c1e6",
+    "url": "/agile-client/static/js/main.3b8e9140.chunk.js"
+  },
+  {
+    "revision": "a42c60fdf136b9ea67d2",
+    "url": "/agile-client/static/js/runtime-main.acd4d2a2.js"
+  },
+  {
+    "revision": "70a0e43f5a74132653d5a5dd4399208e",
+    "url": "/agile-client/static/media/clients-2-dark-teal.70a0e43f.svg"
+  },
+  {
+    "revision": "aab4b42fc133d05d5c4de91a94cd0081",
+    "url": "/agile-client/static/media/signin-image-3.aab4b42f.svg"
+  },
+  {
+    "revision": "062200e7dacfc975a7bc00ee9f856372",
+    "url": "/agile-client/static/media/signup-image-1.062200e7.svg"
+  }
+]);
